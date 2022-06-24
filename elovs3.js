81ac46bdd52c7543c555329b39857de59298a11f
maeztrow12 <97584343+maeztrow12@users.noreply.github.com>
2022-06-24 08:22:10 +0700

    Update elovs3.js

--- elovs3.js
+++ elovs3.js
@@ -74,8 +74,8 @@ function kirim_wa() {
   queenie += "\n\n", queenie += macartney, queenie += donnesha(660), queenie += config[donnesha(568)].checkout_subtotal + donnesha(687) + format_currency(jesilyn.subtotal) + "\n";
   jesilyn[donnesha(694)] && (queenie += config[donnesha(568)][donnesha(658)] + " : " + format_currency(jesilyn[donnesha(694)]) + "\n");
   queenie += "\n" + config[donnesha(568)][donnesha(649)] + donnesha(823) + format_currency(jesilyn[donnesha(581)]) + "*", queenie += "\n\n————————————————", queenie += "\n\n", queenie += "*" + config[donnesha(568)][donnesha(640)] + donnesha(621), queenie += jesilyn[donnesha(454)][donnesha(508)] + "\n", queenie += jesilyn[donnesha(454)][donnesha(439)] + "\n\n", queenie += "*" + config[donnesha(568)].checkout_payment + donnesha(621), queenie += jesilyn.pembayaran[donnesha(508)] + "\n", queenie += jesilyn[donnesha(731)][donnesha(452)] + "\n\n", queenie += "*" + config[donnesha(568)][donnesha(569)] + donnesha(621), queenie += jesilyn[donnesha(771)].nama + donnesha(351) + jesilyn.penerima[donnesha(816)] + donnesha(762), queenie += jesilyn[donnesha(771)][donnesha(565)] + "\n";
-  jesilyn[donnesha(771)][donnesha(369)] && (queenie += "\nNama Produk di paket : \n", queenie += jesilyn.penerima[donnesha(369)] + "");
-    jesilyn[donnesha(771)][donnesha(369)] && (queenie += "\n\nNama Pengirim di paket : \n", queenie += view_pengirim + "");
+  jesilyn[donnesha(771)][donnesha(369)] && (queenie += "\nNama Produk di ganti : \n", queenie += jesilyn.penerima[donnesha(369)] + "");
+    jesilyn[donnesha(771)][donnesha(369)] && (queenie += "\n\nNama Pengirim di ganti : \n", queenie += view_pengirim + "");
 
   queenie += "\n\n", queenie += donnesha(444) + config.home_url, delete localStorage[donnesha(634)];
   var linsie = donnesha(665) + config[donnesha(578)] + parseInt(config[donnesha(562)]) + donnesha(382)+ encodeURIComponent(queenie);
