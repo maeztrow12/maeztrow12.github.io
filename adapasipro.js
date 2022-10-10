3f0da351084a577a16557fe769ae7a6a9a6f6760
maeztrow12 <97584343+maeztrow12@users.noreply.github.com>
2022-10-10 13:20:46 +0700

    Update adapasipro.js

--- adapasipro.js
+++ adapasipro.js
@@ -15,7 +15,8 @@ window[raice(517)] = function () {
   etc(), slideshow(), product_convert(), product_sort(), shortcode(), cart(), lazyload(), lightbox(), pop(), popwin(), timeago(), translate(), custom_js();
 };
 function cart() {
-  var tannyr = raice, brinleigh = tannyr(561) + $_config[tannyr(710)][tannyr(711)] + tannyr(326) + $_config[tannyr(710)].checkout_phone + tannyr(640);
+  var tannyr = raice, brinleigh = tannyr(561) +  $_config[tannyr(667)][tannyr(265)] && (brinleigh += '<p>penjaual</p>');
+ $_config[tannyr(710)][tannyr(711)] + tannyr(326) + $_config[tannyr(710)].checkout_phone + tannyr(640);
   $_config[tannyr(667)].address && (brinleigh += tannyr(436) + $_config[tannyr(710)].checkout_address + tannyr(652));
  $_config[tannyr(667)][tannyr(265)] && (brinleigh += '<p>penjaual</p>');
   $_config[tannyr(667)][tannyr(265)] && (brinleigh += '            <input type="number" name="email" placeholder="' + $_config[tannyr(710)][tannyr(383)] + tannyr(447));
