a5ec354f6e0e65d2716a94bba2549aa28af7591c
maeztrow12 <97584343+maeztrow12@users.noreply.github.com>
2023-02-07 00:31:05 +0700

    Update Latanzaclusterdemak.js

--- Latanzaclusterdemak.js
+++ Latanzaclusterdemak.js
@@ -1,3 +1,30 @@
+function _0x4ef1() {
+  var _0x1fb51e = ["&sz=32", ".blogger.com", "<b>Kode Lisensi</b> dibutuhkan..<br><br><small>#ERR_LICENSE</small>", "1244848HYULIS", ".blogspot.com", "225372ZpJnUb", "//m.", '<b>Hargailah Hak Cipta</b><br><br><small>Gunakan "Lisensi Premium" untuk menghapus credit.</small><br><br><small>#ERR_COPYRIGHT</small>', "</b><br><br><small>#ERR_DOMAIN</small>", "onload", "each", "data-src", "//www.", "2iqTebY", "//api.", "toUpperCase", "slice", "getElementById", "#highlight", '" target="_blank"><b>Kr</b></a>', "395073VOQlMm", "42bzEWdH", "[data-bg]", "861435YlfeWZ", "url(", '" target="_blank" style="opacity:.7;font-size:70%;letter-spacing:1px;">https://lapak.kangrian.net</a>                </div>            </div>        ', "https://www.google.com/s2/favicons?domain=", "style", "replace", "206934BfTClw", "detail", "src", "3268600YazBGv", "XXX", "<b>Kode Lisensi</b> yang digunakan tidak valid.</b><br><br><small>#ERR_KEY</small>", "setAttribute", "getFullYear", "replaceAll", "originalEvent", "wheelDelta", "1917558uCZcLS", "attr", ' &#8212; Made with. <a href="https://lapak.kangrian.net/', "innerHTML", "load", "DOMMouseScroll mousewheel", "scrollLeft", "20VIXAgk", "split", "charAt", "data-bg", "removeAttr", "bind", "indexOf", "3xPgKCw", "linkpage-blogger-template", '            <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">            <style>hr { margin:0!important; } body { padding: 0px!important; } * { font-family: "Rubik", sans-serif; font-weight:400; box-sizing:border-box;margin:0;padding:0; text-decoration:none; color:#636363; } b { font-weight: 500; color:#232323; }</style>            <div id="notice_html" style="display:flex;background:#fafafa;min-height:100vh;text-align:center;">                <div style="margin:auto;width:480px;max-width:80%;background:white;padding:30px;border-radius:10px;border:1px solid #ddd;">                    <svg style="fill:#636363;width:100px;height:100px;display:block;margin:0 auto 20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.9 79.9L51.8 403.1C40.6 423.3 55.5 448 78.9 448h354.3c23.3 0 38.2-24.7 27.1-44.9L283.1 79.9c-11.7-21.2-42.5-21.2-54.2 0zM273.6 214L270 336h-28l-3.6-122h35.2zM256 402.4c-10.7 0-19.1-8.1-19.1-18.4s8.4-18.4 19.1-18.4 19.1 8.1 19.1 18.4-8.4 18.4-19.1 18.4z"/></svg>                    <h2><b>Peringatan</b> :</h2>                    <br>                    <p>', "hostname", "undefined", '<img src="', "preventDefault", "body", "[data-src]", "<b>Kode Lisensi</b> hanya bisa digunakan untuk <b>", "#socmed a, #custom a"];
+  _0x4ef1 = function () {
+    return _0x1fb51e;
+  };
+  return _0x4ef1();
+}
+var _0x3f9648 = _0x1122;
+function _0x1122(_0x5f0a10, _0x7ca50b) {
+  var _0x4ef10b = _0x4ef1();
+  return _0x1122 = function (_0x112233, _0x495f44) {
+    _0x112233 = _0x112233 - 449;
+    var _0x59e633 = _0x4ef10b[_0x112233];
+    return _0x59e633;
+  }, _0x1122(_0x5f0a10, _0x7ca50b);
+}
+(function (_0x3dd883, _0x4a4795) {
+  var _0x5269f1 = _0x1122, _0x1789f4 = _0x3dd883();
+  while (true) {
+    try {
+      var _0x517e33 = parseInt(_0x5269f1(474)) / 1 * (-parseInt(_0x5269f1(467)) / 2) + -parseInt(_0x5269f1(508)) / 3 * (parseInt(_0x5269f1(459)) / 4) + parseInt(_0x5269f1(477)) / 5 + parseInt(_0x5269f1(483)) / 6 * (-parseInt(_0x5269f1(475)) / 7) + parseInt(_0x5269f1(486)) / 8 + -parseInt(_0x5269f1(494)) / 9 * (-parseInt(_0x5269f1(501)) / 10) + -parseInt(_0x5269f1(457)) / 11;
+      if (_0x517e33 === _0x4a4795) break; else _0x1789f4.push(_0x1789f4.shift());
+    } catch (_0x240dd9) {
+      _0x1789f4.push(_0x1789f4.shift());
+    }
+  }
+}(_0x4ef1, 235468));
 
 window[_0x3f9648(463)] = function () {
   etc(), custom_js();
